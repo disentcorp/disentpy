981bdd8f4cb84efd4241ac245a5006c2f64b4aaa
amalizzio <anthony.malizzio@disent.com>
2022-06-08 19:42:47 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='disent',
+    version='0.0.1',
+    description='Disent API caller',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Niels lauritzen',
+    author_email='niels.lauritzen@disent.com',
+    license='unlicense',
+    packages=['disent'],
+    zip_safe=False
+)
